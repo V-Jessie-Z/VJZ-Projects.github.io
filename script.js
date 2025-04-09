f6cf2e74dd9be1ed87d6305e95c8579b62340d66
Varaidzo Jessica Zimondi <116093000+V-Jessie-Z@users.noreply.github.com>
2025-04-09 14:33:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function scrollToSection(sectionId) {
   document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
 
+function scrollProjects(direction) {
+    const container = document.querySelector('.project-slider');
+    const scrollAmount = 300;
+    container.scrollBy({
+        left: direction === 'left' ? -scrollAmount : scrollAmount,
+        behavior: 'smooth'
+    });
+}
